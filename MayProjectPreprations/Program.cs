14f3c3dfa6ad4a46908cd5132d206dc57cc90966
Hussnain <hasni.rock@yahoo.com>
2024-04-25 16:08:55 +0500

    Add project files.

---
+++ MayProjectPreprations/Program.cs
@@ -0,0 +1,13 @@
+﻿// 1- Print Hello World using Console.
+using System;
+
+namespace MayProjectPreprations
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
